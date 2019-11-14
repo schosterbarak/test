aeb67c7bf45e35b339965a33f50deb808fd19383
Barak Schoster Goihman <schosterbarak@gmail.com>
2019-11-14 11:54:36 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
+}
+
+resource "aws_security_group_rule" "my-rule" {
+  type        = "ingress"
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port = 22
+  protocol = "tcp"
+  security_group_id = "sg-11"
+  to_port = 22
+}
