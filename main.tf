474dc54a3d95166c5978e909386a6bb0f619aba4
Barak Schoster Goihman <schosterbarak@gmail.com>
2019-11-14 11:54:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,6 +8,6 @@ resource "aws_security_group_rule" "my-rule" {
   cidr_blocks = ["0.0.0.0/0"]
   from_port = 22
   protocol = "tcp"
-  security_group_id = "sg-11"
+  security_group_id = "sg-123456"
   to_port = 22
 }
